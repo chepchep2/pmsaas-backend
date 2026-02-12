31887e3a13307ee90c7628b5c7072caa982674f2
chepchep2 <kajer102@gmail.com>
2026-02-12 22:46:34 +0900

    feat: repository 메서드 추가

--- src/main/java/com/chep/demo/todo/domain/task/TaskRepository.java
+++ src/main/java/com/chep/demo/todo/domain/task/TaskRepository.java
@@ -1,6 +1,8 @@
 package com.chep.demo.todo.domain.task;
 
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -8,9 +10,16 @@ import java.util.Optional;
 public interface TaskRepository extends JpaRepository<Task, Long> {
     List<Task> findAllByUserIdOrderByOrderIndexAsc(Long userId);
     Optional<Task> findByIdAndUserId(Long id, Long userId);
-    Long countByUserId(Long userId);
+    Long countByProjectId(Long projectId);
     List<Task> findByUserIdAndOrderIndexBetween(Long userId, int start, int end);
     List<Task> findByUserIdAndOrderIndexGreaterThan(Long userId, int start);
+    List<Task> findByProjectIdAndOrderIndexBetween(Long projectId, int start, int end);
+    List<Task> findAllByProjectIdOrderByOrderIndexAsc(Long projectId);
+    List<Task> findByProjectIdAndOrderIndexGreaterThan(Long projectId, int start);
+    @Query("SELECT t FROM Task t " +
+            "WHERE t.project.workspace.id = :workspaceId " +
+            "ORDER BY t.project.id, t.orderIndex")
+    List<Task> findAllByWorkspaceId(@Param("workspaceId") Long workspaceId);
 
     default void softDelete(Task task) {
         task.markDeleted();

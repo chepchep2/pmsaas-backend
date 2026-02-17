a9075c5ebbf07203bf6d167941b8b628af5d2e1b
chepchep2 <kajer102@gmail.com>
2026-02-17 17:53:50 +0900

    feat: RedisKeys 경로 수정 및 notification queue, processing 추가

---
+++ src/main/java/com/chep/demo/todo/service/RedisKeys.java
@@ -0,0 +1,7 @@
+package com.chep.demo.todo.service;
+
+public class RedisKeys {
+    public static final String INVITATION_QUEUE = "invitation:queue";
+    public static final String NOTIFICATION_QUEUE = "notification:queue";
+    public static final String NOTIFICATION_PROCESSING = "notification:processing";
+}

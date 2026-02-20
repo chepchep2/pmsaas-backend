52c3a4fc9700f90b9d33fa00408e4160bd1d0f5b
chepchep2 <kajer102@gmail.com>
2026-02-20 18:17:52 +0900

    refactor: ResClient로 수정
    - RestClientConfig 추가하여 타임아웃 설정

--- src/main/java/com/chep/demo/todo/service/notification/SlackClient.java
+++ src/main/java/com/chep/demo/todo/service/notification/SlackClient.java
@@ -3,7 +3,11 @@ package com.chep.demo.todo.service.notification;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Map;
@@ -11,21 +15,28 @@ import java.util.Map;
 @Service
 public class SlackClient {
     private static final Logger log = LoggerFactory.getLogger(SlackClient.class);
-    private final WebClient webClient;
+    private final RestClient restClient;
 
-    public SlackClient(@Value("${slack.webhook-url}") String webhookUrl) {
-        this.webClient = WebClient.builder()
-                .baseUrl(webhookUrl)
-                .build();
+    public SlackClient(RestClient.Builder restClientBuilder, @Value("${slack.webhook-url}") String webhookUrl) {
+        this.restClient = restClientBuilder.baseUrl(webhookUrl).build();
     }
 
     public void send(String message) {
         log.info("Sending Slack message: {}", message);
-        webClient.post()
-                .bodyValue(Map.of("text", message))
-                .retrieve()
-                .bodyToMono(String.class)
-                .block();
-        log.info("Slack message sent successfully");
+        try {
+            String responseBody = restClient.post()
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(Map.of("text", message))
+                    .retrieve()
+                    .body(String.class);
+            log.info("Slack message sent. response={}", responseBody);
+
+        } catch (RestClientResponseException e) {
+            log.error("Slack responded with error. status={} body={}", e.getStatusCode(), e.getResponseBodyAsString(), e);
+            throw e;
+        } catch (ResourceAccessException e) {
+            log.error("Slack request failed due to I/O issue (timeout/connection).", e);
+            throw e;
+        }
     }
 }

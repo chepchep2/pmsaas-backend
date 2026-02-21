c3f571900a3123e90ed172e71dfc7c5dc86b3297
chepchep2 <kajer102@gmail.com>
2026-02-21 16:19:46 +0900

    feat: JpaRepository 상속 빼먹은 것 추가

--- src/main/java/com/chep/demo/todo/domain/task/TaskAssigneeRepository.java
+++ src/main/java/com/chep/demo/todo/domain/task/TaskAssigneeRepository.java
@@ -1,11 +1,12 @@
 package com.chep.demo.todo.domain.task;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface TaskAssigneeRepository {
+public interface TaskAssigneeRepository extends JpaRepository<TaskAssignee, Long> {
     @Query("""
             SELECT ta
             FROM TaskAssignee ta

94a0aa65f94a342feeb9e43e3729c1e8b29a21e9
chepchep2 <kajer102@gmail.com>
2026-02-25 00:34:41 +0900

    feat: retry 키, 커스텀 예외 추가

--- src/main/java/com/chep/demo/todo/service/RedisKeys.java
+++ src/main/java/com/chep/demo/todo/service/RedisKeys.java
@@ -4,4 +4,6 @@ public class RedisKeys {
     public static final String INVITATION_QUEUE = "invitation:queue";
     public static final String NOTIFICATION_QUEUE = "notification:queue";
     public static final String NOTIFICATION_PROCESSING = "notification:processing";
+    public static final String RETRY_QUEUE = "notification:retry:queue";
+    public static final String RETRY_COUNT_PREFIX = "notification:retry:";
 }
